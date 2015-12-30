89256a00b1e0d0d8d5da7412c0e9a81aefab79a9
David Choo <davidchoo221@gmail.com>
2015-12-30 17:03:29 -0600

    Update media.php

--- public/media.php
+++ public/media.php
@@ -4,52 +4,50 @@
 
 	<h1>File Name</h1>
 
-	<audio controls src = "C:\Users\David Choo\Downloads\HONNE - Warm On A Cold Night.mp3"></audio>
+	<audio controls src = "C:\Users\David Choo\Downloads\HONNE - Warm On A Cold Night.mp3"></audio> <!--This is a temporary file - replace with file pulled from database -->
 
 	<h2>General</h2>
 
 	Project Name: 
 	<br><br>
-</input>
+	Song Title:
+	<br><br>
 
-Song Title:
-<br><br>
+	Artist:
+	<br><br>
 
-Artist:
-<br><br>
+	Genre:
+	<br><br>
 
-Genre:
-<br><br>
+	Theme:
+	<br><br>	
 
-Theme:
-<br><br>	
+	<h2>Location</h2>
 
-<h2>Location</h2>
+	Continent:
+	<br><br>
 
-Continent:
-<br><br>
+	Country:
+	<br><br>
 
-Country:
-<br><br>
+	Region:
+	<br><br>
 
-Region:
-<br><br>
+	<h2>Other</h2>
 
-<h2>Other</h2>
+	Tagwords:
+	<br><br>
 
-Tagwords:
-<br><br>
+	Copyright:
+	<br><br>
 
-Copyright:
-<br><br>
+	Publisher:
+	<br><br>
 
-Publisher:
-<br><br>
+	iTunes Link:
+	<br><br>
 
-iTunes Link:
-<br><br>
-
-Amazon Link:
-<br><br>
+	Amazon Link:
+	<br><br>
 
 <?php include "footer.php"; ?>
