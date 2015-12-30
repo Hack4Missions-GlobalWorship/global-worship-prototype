94666dfcf89f490e2dd1e800321fcb8cb532d7d7
David Choo <davidchoo221@gmail.com>
2015-12-30 17:00:51 -0600

    Create media.php

---
+++ public/media.php
@@ -0,0 +1,55 @@
+<?php include "header.php"; ?>
+
+	<title>File Name</title>
+
+	<h1>File Name</h1>
+
+	<audio controls src = "C:\Users\David Choo\Downloads\HONNE - Warm On A Cold Night.mp3"></audio>
+
+	<h2>General</h2>
+
+	Project Name: 
+	<br><br>
+</input>
+
+Song Title:
+<br><br>
+
+Artist:
+<br><br>
+
+Genre:
+<br><br>
+
+Theme:
+<br><br>	
+
+<h2>Location</h2>
+
+Continent:
+<br><br>
+
+Country:
+<br><br>
+
+Region:
+<br><br>
+
+<h2>Other</h2>
+
+Tagwords:
+<br><br>
+
+Copyright:
+<br><br>
+
+Publisher:
+<br><br>
+
+iTunes Link:
+<br><br>
+
+Amazon Link:
+<br><br>
+
+<?php include "footer.php"; ?>
